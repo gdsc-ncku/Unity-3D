37759134eb8d920a0731e13c0a6c5af7f58b898e
Aka2210 <qoo0973215128@gmail.com>
2024-01-23 17:40:54 +0800

    Add script about perspective shift

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public bool _lock;
+    [SerializeField] GameObject _cameraRoot;
+    [SerializeField] float _mouseSensitivity, _cameraMovingThreshold;
+    float _mouseXInput, _mouseYInput;
+
+    void Update()
+    {
+        //Click right mouse to hide cursor or press ESC to display cursor
+        if(Input.GetMouseButtonDown(0))
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Grab mouse movement
+        _mouseXInput = Input.GetAxis("Mouse X");
+        _mouseYInput = Input.GetAxis("Mouse Y");
+
+        //Grab present rotation
+        float _rotateX = _cameraRoot.transform.eulerAngles.x, _rotateY = _cameraRoot.transform.eulerAngles.y;
+
+        //The range of _rotateX is 0~360, therefore we need if,else if to let its range limit in -180~180
+        if (_rotateX > 180)
+            _rotateX -= 360;
+        else if (_rotateX < -180)
+            _rotateX += 360;
+
+        //Detect whether camera open and input >= threshold
+        if (!_lock && (Mathf.Abs(_mouseXInput) >= _cameraMovingThreshold || Mathf.Abs(_mouseYInput) >= _cameraMovingThreshold))
+        {
+            _rotateX -= _mouseYInput * _mouseSensitivity * Time.deltaTime;
+            _rotateY += _mouseXInput * _mouseSensitivity * Time.deltaTime;
+        }
+
+        //Limit camera rotation
+        _rotateX = ClampAngle(_rotateX, -70, 70);
+        _rotateY = ClampAngle(_rotateY, -360, 360);
+
+        //Rotate camera
+        _cameraRoot.transform.rotation = Quaternion.Euler(_rotateX, _rotateY, 0);
+    }
+
+    float ClampAngle(float Angle, float Min, float Max)
+    {
+        if (Angle < -360f) Angle += 360f;
+        if (Angle > 360f) Angle -= 360f;
+        return Mathf.Clamp(Angle, Min, Max);
+    }
+}
