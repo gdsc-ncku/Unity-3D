42dce294a0c811e96dd20bbfdbc21b560b46b77f
Wei Ting Huang <f64101032@gs.ncku.edu.tw>
2024-03-03 20:52:50 +0800

    bulletground

--- Assets/Scripts/Controller/weapon/gun/bulletcontroller.cs
+++ Assets/Scripts/Controller/weapon/gun/bulletcontroller.cs
@@ -7,6 +7,7 @@ public class bulletcontroller : MonoBehaviour
     //武器屬性
     [SerializeField]
     WeaponData gun;
+    private float downtime=0.8f; //落下時間(高度)
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +15,12 @@ public class bulletcontroller : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(gameObject,0.5f);
+        Destroy(gameObject);
+        //碰撞後的傷害可以放在這裡
     }
     // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
+    void Update()
+    {
+        Destroy(gameObject,downtime);
+    }
 }
