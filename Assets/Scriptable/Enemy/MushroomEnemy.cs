86c1cdcb7f9bb8ce2a8436b2ad6514acedabffa0
Aka2210 <qoo0973215128@gmail.com>
2024-05-31 09:02:29 +0800

    feat: Add mushroom enemy

--- Assets/Scriptable/Enemy/MushroomEnemy.cs
+++ Assets/Scriptable/Enemy/MushroomEnemy.cs
@@ -5,8 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MushroomEnemy", menuName = "Enemy/MushroomEnemy", order = 5)]
 public class MushroomEnemy : EnemyScriptableObject
 {
-    public override void attack(GameObject enemy)
-    {
-        Instantiate(Bullets[Random.Range(0, Bullets.Length)], enemy.transform.position, enemy.transform.rotation).GetComponent<ChestEnemyBullets>().Spawner = enemy;
-    }
+
 }
