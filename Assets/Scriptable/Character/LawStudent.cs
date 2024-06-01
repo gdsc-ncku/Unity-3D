b14240be08acb5f0609a470e42c8957c3608b038
zeroazu <kuovivian993@gmail.com>
2024-06-01 16:48:19 +0800

    fix: skill

--- Assets/Scriptable/Character/LawStudent.cs
+++ Assets/Scriptable/Character/LawStudent.cs
@@ -5,31 +5,37 @@ public class LawStudent : CharacterBaseData
 {
     // You can create specific strengthening below.
     #region Hero_Q_Skill
-    public float range = 10f;
+    public float QSkillRange = 10f;
     #endregion
 
     #region Hero_E_Skill
-    public float range = 10f;
+    public float ESkillRange = 10f;
     #endregion
 
-    // Hero skills effect need to write in the functions below.
-    public new void UseingQ_Skill(Transform player, LayerMask whatIsEnemy ,GameObject effectPrefab)
+    // document falls around and hits the enemy
+    public void UseingQ_Skill(Transform player, LayerMask whatIsEnemy, GameObject effectPrefab)
     {
-        Instantiate(effectPrefab, player, Quaternion.identity);
-        if (Physics.Raycast(ray, out hit, range, whatIsEnemy))
+        // Instantiate effect at player position
+        Instantiate(effectPrefab, player.position, Quaternion.identity);
+
+        // Find all enemies within range
+        Collider[] enemiesInRange = Physics.OverlapSphere(player.position, QSkillRange, whatIsEnemy);
+
+        // Log the names of all enemies within range
+        foreach (Collider enemy in enemiesInRange)
         {
-            Debug.Log("Skill activated at enemy position: " + hit.point);
+            Debug.Log("Enemy in range: " + enemy.gameObject.name);
         }
     }
 
-    // This method combines skill activation.
+    // Throw the gavel at the enemy
     public void UseingE_Skill(Transform cam, Transform attackPoint, LayerMask whatIsEnemy, GameObject effectPrefab)
     {
         // Cast a ray to detect enemy
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, range, whatIsEnemy))
+        if (Physics.Raycast(ray, out hit, ESkillRange, whatIsEnemy))
         {
             // Instantiate effect at enemy position
             Instantiate(effectPrefab, hit.point, Quaternion.identity);
--- Assets/Scriptable/Character/MEStudent.cs
+++ Assets/Scriptable/Character/MEStudent.cs
@@ -3,23 +3,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MEStudent", menuName = "Character/MEStudent")]
 public class MEStudent : CharacterBaseData
 {
-    // You can create specific strengthening below.
     #region Hero_Q_Skill
-    public float range = 10f;
+    public float QskillRange = 10f;
     #endregion
 
     #region Hero_E_Skill
-    
+
     #endregion
 
-    // Hero skills effect need to write in the functions below.
-    public new void UseingQ_Skill()
+    // The turret falls and hits the enemy
+    public void UseingQ_Skill(Transform cam, Transform attackPoint, LayerMask whatIsEnemy, GameObject effectPrefab)
     {
         // Cast a ray to detect enemy
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, range, whatIsEnemy))
+        if (Physics.Raycast(ray, out hit, QskillRange, whatIsEnemy))
         {
             // Instantiate effect at enemy position
             Instantiate(effectPrefab, hit.point, Quaternion.identity);
