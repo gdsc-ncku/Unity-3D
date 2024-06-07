185285e8768be954415b1b754f1e46960dd4c0d0
Aka2210 <qoo0973215128@gmail.com>
2024-06-07 14:30:51 +0800

    fix: Fix change key bug

--- Assets/Scripts/Controller/MainScene/ChangeKey.cs
+++ Assets/Scripts/Controller/MainScene/ChangeKey.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeKey : MonoBehaviour
 {
@@ -15,27 +16,25 @@ public class ChangeKey : MonoBehaviour
 
     public void changeMoveKey()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         KeyIndex key;
-        if (bindingIndex == 1)
+        switch (bindingIndex)
         {
-            key = KeyIndex.Forward;
-            PlayerPrefs.SetString(KeyIndex.Forward.ToString(), text.text);
-        }
-        else if (bindingIndex == 2)
-        {
-            key = KeyIndex.Backward;
-        }
-        else if (bindingIndex == 3)
-        {
-            key = KeyIndex.Left;
-        }
-        else if(bindingIndex == 4)
-        {
-            key = KeyIndex.Right;
-        }
-        else
-        {
-            return;
+            case 1:
+                key = KeyIndex.Forward;
+                break;
+            case 2:
+                key = KeyIndex.Backward;
+                break;
+            case 3:
+                key = KeyIndex.Left;
+                break;
+            case 4:
+                key = KeyIndex.Right;
+                break;
+            default:
+                return;
         }
 
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.Move, bindingIndex, text, key);
@@ -43,51 +42,71 @@ public class ChangeKey : MonoBehaviour
 
     public void changeSpaceKey()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.Jump, bindingIndex, text, KeyIndex.Jump);
     }
 
     public void changeFireKey()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.Fire, bindingIndex, text, KeyIndex.Fire);
     }
 
     public void changeWeapon1Key()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.Weapon1, bindingIndex, text, KeyIndex.Weapon1);
     }
 
     public void changeWeapon2Key()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.Weapon2, bindingIndex, text, KeyIndex.Weapon2);
     }
 
     public void changeQ_SkillKey()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.Q_Skill, bindingIndex, text, KeyIndex.Q_Skill);
     }
 
     public void changeE_SkillKey()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.E_Skill, bindingIndex, text, KeyIndex.E_Skill);
     }
 
     public void changeBackpackKey()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.Backpack, bindingIndex, text, KeyIndex.Backpack);
     }
 
     public void changeReloadKey()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.Reload, bindingIndex, text, KeyIndex.Reload);
     }
 
     public void changeWeapomSkillKey()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.WeaponSkill, bindingIndex, text, KeyIndex.WeaponSkill);
     }
 
     public void changePickUpKey()
     {
+        gameObject.transform.GetChild(1).GetComponent<Button>().enabled = false;
+
         settingController.ChangeKey(settingController.settingManager.playerBasicInformationScriptable.playerControl.Player, settingController.settingManager.playerBasicInformationScriptable.playerControl.Player.PickUp, bindingIndex, text, KeyIndex.PickUp);
     }
 }
--- Assets/Scripts/Manager/MainScene/SettingManager.cs
+++ Assets/Scripts/Manager/MainScene/SettingManager.cs
@@ -7,9 +7,4 @@ public class SettingManager : MonoBehaviour
     public List<keyboard> keyboards = new List<keyboard>();
     public List<string> keyboardDisplayKey = new();
     public List<string> keyboardDisplayValue = new();
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 }
